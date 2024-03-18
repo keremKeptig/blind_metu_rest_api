d08de3830b8fdd89fd05b1eee83f4d904f4c0bd7
keremKeptig <kkeptig@gmail.com>
2024-03-18 17:55:54 +0200

    userEndpoint

--- models/test.py
+++ models/test.py
@@ -8,6 +8,6 @@ class TestTable(db.Model):
     type = db.Column(db.String(80), unique=False, nullable=False)
     test_date = db.Column(db.String(256), nullable=False)
 
-    questions = db.relationship("QuestionsTable", back_populates="test", lazy="dynamic", cascade="all, delete")
+    questions = db.relationship("QuestionsTable", back_populates="tests", lazy="dynamic", cascade="all, delete")
 
 
