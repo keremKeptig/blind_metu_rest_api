d0e11097c20e1e6fa750b772d60387e45ea9ef73
keremKeptig <kkeptig@gmail.com>
2024-03-18 18:26:15 +0200

    userEndpoint

--- schemas.py
+++ schemas.py
@@ -1,7 +1,7 @@
 from marshmallow import Schema, fields
 
 
-class UserSchema(Schema):
+class UserRegisterSchema(Schema):
     id = fields.Int(dump_only=True)
     username = fields.Str(required=True)
     # Load only because of processing passwords secretly
@@ -11,20 +11,20 @@ class UserSchema(Schema):
     sexual_interest = fields.Int(required=False)
 
 
-class PlainAnswer(Schema):
-    id = fields.Int(dump_only=True)
-    user_id = fields.Int(dump_only=True)
+# class PlainAnswer(Schema):
+#     id = fields.Int(dump_only=True)
+#     user_id = fields.Int(dump_only=True)
+#
+#
+# class PlainMatches(Schema):
+#     id = fields.Int(dump_only=True)
+#     user_id = fields.Int(dump_only=True)
+# class PlainQuestions(Schema):
+#     id = fields.Int(dump_only=True)
+#     user_id = fields.Int(dump_only=True)
 
-
-class PlainMatches(Schema):
-    id = fields.Int(dump_only=True)
-    user_id = fields.Int(dump_only=True)
-class PlainQuestions(Schema):
-    id = fields.Int(dump_only=True)
-    user_id = fields.Int(dump_only=True)
-
-class UserRegisterSchema(UserSchema):
-    answers = fields.List(fields.Nested(PlainAnswer()), dump_only=True)
-    matches = fields.List(fields.Nested(PlainMatches()), dump_only=True)
-    questions = fields.List(fields.Nested(PlainQuestions()), dump_only=True)
-
+# class UserRegisterSchema(UserSchema):
+#     answers = fields.List(fields.Nested(PlainAnswer()), dump_only=True)
+#     matches = fields.List(fields.Nested(PlainMatches()), dump_only=True)
+#     questions = fields.List(fields.Nested(PlainQuestions()), dump_only=True)
+#
