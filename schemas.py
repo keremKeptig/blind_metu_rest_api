51e068d6cf9ae5ea289b7e446665ec778f217131
keremKeptig <kkeptig@gmail.com>
2024-03-18 18:53:31 +0200

    userEndpoint

--- schemas.py
+++ schemas.py
@@ -5,12 +5,11 @@ class UserSchema(Schema):
     id = fields.Int(dump_only=True)
     username = fields.Str(required=True)
     # Load only because of processing passwords secretly
-
+    password = fields.Str(required=True, load_only=True)
 
 
 
 class UserRegisterSchema(UserSchema):
-    password = fields.Str(required=True, load_only=True)
     gender = fields.Int(required=True)
     date_of_birth = fields.Str(required=False)
     sexual_interest = fields.Int(required=False)
