2efacaec160df5257c4398f49a52c5d8b3c2ec4f
keremKeptig <kkeptig@gmail.com>
2024-03-18 18:31:38 +0200

    userEndpoint

--- schemas.py
+++ schemas.py
@@ -1,30 +1,18 @@
 from marshmallow import Schema, fields
 
 
-class UserRegisterSchema(Schema):
+class UserSchema(Schema):
     id = fields.Int(dump_only=True)
     username = fields.Str(required=True)
     # Load only because of processing passwords secretly
+
+
+
+
+class UserRegisterSchema(UserSchema):
     password = fields.Str(required=True, load_only=True)
     gender = fields.Int(required=True)
     date_of_birth = fields.Str(required=False)
     sexual_interest = fields.Int(required=False)
 
 
-# class PlainAnswer(Schema):
-#     id = fields.Int(dump_only=True)
-#     user_id = fields.Int(dump_only=True)
-#
-#
-# class PlainMatches(Schema):
-#     id = fields.Int(dump_only=True)
-#     user_id = fields.Int(dump_only=True)
-# class PlainQuestions(Schema):
-#     id = fields.Int(dump_only=True)
-#     user_id = fields.Int(dump_only=True)
-
-# class UserRegisterSchema(UserSchema):
-#     answers = fields.List(fields.Nested(PlainAnswer()), dump_only=True)
-#     matches = fields.List(fields.Nested(PlainMatches()), dump_only=True)
-#     questions = fields.List(fields.Nested(PlainQuestions()), dump_only=True)
-#
