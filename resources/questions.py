4c92e43d81bd58643e42fb6efb395ccc44f34643
keremKeptig <kkeptig@gmail.com>
2024-03-25 16:12:18 +0200

    answerMake

--- resources/questions.py
+++ resources/questions.py
@@ -8,6 +8,7 @@ from passlib.hash import pbkdf2_sha256
 from database import db
 from schemas import UserSchema, QuestionSchema
 from models import QuestionTable,TestTable
+from datetime import datetime
 import jinja2
 from datetime import date
 # Creating a Flask-Smorest Blueprint
@@ -27,14 +28,29 @@ class Question(MethodView):
         question_texts = [(question.q_text, question.q_id) for question in questions]
         return jsonify(question_texts)
 
-@blp.route("/question/<string:current_date>")
+@blp.route("/question")
 class TestFind(MethodView):
 
     @blp.response(200)
-    def get(self, current_date):
-        # tests = TestTable.query.all()
+    def get(self):
+        # tests = T estTable.query.all()
+        tests = TestTable.query.all()
 
-        max_test_id = db.session.query(func.max(TestTable.test_id)).scalar()
-        print("En yüksek test ID:", max_test_id)
+        date_format = "%Y/%m-%d"  # Dizeyi bu formata göre çözümleme
 
-        return max_test_id
+        test_id = -1
+
+        current_datetime = datetime.now()
+        date_object = datetime.strptime(current_datetime.strftime("%Y-%m-%d"), "%Y-%m-%d")
+
+        for test in tests:
+            test_start_datetime = datetime.combine(test.test_start_date, datetime.min.time())
+            test_end_datetime = datetime.combine(test.test_end_date, datetime.max.time())
+
+            # Karşılaştırma yapın
+            if test_start_datetime <= date_object <= test_end_datetime:
+                test_id = test.test_id
+                break
+
+
+        return test_id
